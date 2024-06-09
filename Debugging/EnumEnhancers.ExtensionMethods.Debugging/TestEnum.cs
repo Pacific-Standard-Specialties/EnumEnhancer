317ed28cfcf962d171f7aea7a61be46e6ee87ade
Brandon Thetford <github@snapsinazfs.com>
2024-06-09 00:34:37 -0700

    Move and rename everything else
    Also Took attributes out of generator

---
+++ Debugging/EnumEnhancers.ExtensionMethods.Debugging/TestEnum.cs
@@ -0,0 +1,13 @@
+﻿using PSS.EnumEnhancers.ExtensionMethods.Attributes;
+
+namespace EnumEnhancer.Debugging;
+
+[GenerateEnumExtensionMethods]
+public enum TestEnum
+{
+  Zero = 0,
+  One,
+  Two = 2,
+  Three,
+  Six = 6
+}
